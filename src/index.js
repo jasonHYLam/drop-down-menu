3573cdd51d70f3d61587c2b6b9b61493a30ea278
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-22 20:36:33 +0100

    Create ToggleDisplay Function
    If element currently has the hidden class, remove it, else add it.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ console.log('p');
 const dropDownContainer = document.querySelector("#drop-down-container");
 dropDownContainer.addEventListener('mouseover', (e)=>{
     console.log(e.target);
+    toggleDisplay(e.target);
 
 })
+
+function toggleDisplay(element) {
+    (element.classList.contains("hidden"))? element.classList.remove("hidden") : element.classList.add("hidden");
+
+}
