d4d50671bd419ef3ab1f26c783d4e69229212cd0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-22 18:26:43 +0100

    Fix Dist Folder Structure
    Previously there was a dist folder within another dist folder.

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ var __webpack_exports__ = {};
   !*** ./src/index.js ***!
   \**********************/
 console.log('o');
+console.log('p');
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGlCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZHJvcC1kb3duLW1lbnUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ28nKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EsaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kcm9wLWRvd24tbWVudS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnbycpO1xuY29uc29sZS5sb2coJ3AnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
