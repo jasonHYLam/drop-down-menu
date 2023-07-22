8e1772caeeaa29f8e540b5d962e10ffa33ff6f2c
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-22 18:37:29 +0100

    Add Hidden Class To Drop Items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./style.css";
 console.log('o');
 console.log('p');
+
+const dropDownContainer = document.querySelector("#drop-down-container");
+dropDownContainer.addEventListener('mouseover', (e)=>{
+    console.log(e.target);
+
+})
