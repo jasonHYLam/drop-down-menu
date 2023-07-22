7ad4e99bcf4d89326b0debeb3a4fa60fc8ee17d3
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-22 21:28:08 +0100

    Create Basic Elements Creator Function

--- src/index.js
+++ src/index.js
@@ -2,19 +2,35 @@ import "./style.css";
 console.log('o');
 console.log('p');
 
-const hoverBar = document.querySelector("#hover-bar");
 
+const createDropDownMenu = () => {
+    const body = document.querySelector('body');
+    const createElement = (elementType, textContent, ...classes) => {
+        const newElement = document.createElement(elementType);
+        newElement.textContent = textContent;
+        console.log(classes);
+
+        classes.map(className => newElement.classList.add(className));
+        return element;
+
+    }
+
+
+
+    const dropDownMenu = createElement('p', 'hidden', 'drop-down');
+    body.appendChild(dropDownMenu);
+
+}
+const hoverBar = document.querySelector("#hover-bar");
 
 hoverBar.addEventListener('mouseover', (e)=>{
     const dropItems = (e.target.closest("#drop-down-container")).querySelectorAll(".drop-item");
     dropItems.forEach((item) =>showElement(item));
-    // console.log(e.target.querySelectorAll(".drop-item"));
 })
 
 hoverBar.addEventListener('mouseout', (e)=>{
     const dropItems = (e.target.closest("#drop-down-container")).querySelectorAll(".drop-item");
     dropItems.forEach((item) =>hideElement(item));
-    // console.log(e.target.querySelectorAll(".drop-item"));
 })
 
 function hideElement(element) {
@@ -24,3 +40,5 @@ function hideElement(element) {
 function showElement(element) {
     element.classList.remove('hidden');
 }
+
+createDropDownMenu();
