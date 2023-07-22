f1fe8cfd3f3ae2202cd0b3647d83d6381d5927a6
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-22 21:34:24 +0100

    Move Hover Functions To DropDownMenu Constructor

--- src/index.js
+++ src/index.js
@@ -11,34 +11,47 @@ const createDropDownMenu = () => {
         console.log(classes);
 
         classes.map(className => newElement.classList.add(className));
-        return element;
+        return newElement;
+    }
+    const container = createElement('div', '', 'drop-down-container');
+    const hoverBarElement = createElement('div', 'Hover on me', 'hover-bar');
+    const item1 = createElement('div', "about", 'drop-down-item', 'hidden')
+    const item2 = createElement('div', "contact", 'drop-down-item', 'hidden')
+    const item3 = createElement('div', "content", 'drop-down-item', 'hidden')
+    const item4 = createElement('div', "wiki", 'drop-down-item', 'hidden')
+    const item5 = createElement('div', "play", 'drop-down-item', 'hidden')
 
+    container.appendChild(hoverBarElement);
+    container.appendChild(item1);
+    container.appendChild(item2);
+    container.appendChild(item3);
+    container.appendChild(item4);
+    container.appendChild(item5);
+
+    body.appendChild(container);
+
+    const hoverBar = document.querySelector(".hover-bar");
+
+    hoverBar.addEventListener('mouseover', (e)=>{
+        const dropItems = (e.target.closest(".drop-down-container")).querySelectorAll(".drop-item");
+        dropItems.forEach((item) =>showElement(item));
+    })
+
+    hoverBar.addEventListener('mouseout', (e)=>{
+        const dropItems = (e.target.closest("#drop-down-container")).querySelectorAll(".drop-item");
+        dropItems.forEach((item) =>hideElement(item));
+    })
+
+    function hideElement(element) {
+        element.classList.add('hidden');
+    }
+
+    function showElement(element) {
+        element.classList.remove('hidden');
     }
 
 
-
-    const dropDownMenu = createElement('p', 'hidden', 'drop-down');
-    body.appendChild(dropDownMenu);
-
-}
-const hoverBar = document.querySelector("#hover-bar");
-
-hoverBar.addEventListener('mouseover', (e)=>{
-    const dropItems = (e.target.closest("#drop-down-container")).querySelectorAll(".drop-item");
-    dropItems.forEach((item) =>showElement(item));
-})
-
-hoverBar.addEventListener('mouseout', (e)=>{
-    const dropItems = (e.target.closest("#drop-down-container")).querySelectorAll(".drop-item");
-    dropItems.forEach((item) =>hideElement(item));
-})
-
-function hideElement(element) {
-    element.classList.add('hidden');
 }
 
-function showElement(element) {
-    element.classList.remove('hidden');
-}
 
 createDropDownMenu();
