d08ad385d1a8bfcfcd03da81b21668f3ef0bc8fa
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-23 14:28:35 +0100

    Hide Drop Items When Leaving Entire Expanded Area
    This requires mouseleave event listener rather than mouseout, and
    using forEach() and querySelectorAll to target all dropDownMenu
    objects.

--- src/dropDownMenu.js
+++ src/dropDownMenu.js
@@ -8,7 +8,7 @@ export const createDropDownMenu = () => {
         classes.map(className => newElement.classList.add(className));
         return newElement;
     }
-    const container = createElement('div', '', 'drop-down-container');
+    // const container = createElement('div', '', 'drop-down-container');
     const hoverBarElement = createElement('div', 'Hover on me', 'hover-bar');
     const item1 = createElement('div', "about", 'drop-down-item', 'hidden')
     const item2 = createElement('div', "contact", 'drop-down-item', 'hidden')
@@ -16,28 +16,45 @@ export const createDropDownMenu = () => {
     const item4 = createElement('div', "wiki", 'drop-down-item', 'hidden')
     const item5 = createElement('div', "play", 'drop-down-item', 'hidden')
 
-    container.appendChild(hoverBarElement);
-    container.appendChild(item1);
-    container.appendChild(item2);
-    container.appendChild(item3);
-    container.appendChild(item4);
-    container.appendChild(item5);
+    // container.appendChild(hoverBarElement);
+    hoverBarElement.appendChild(item1);
+    hoverBarElement.appendChild(item2);
+    hoverBarElement.appendChild(item3);
+    hoverBarElement.appendChild(item4);
+    hoverBarElement.appendChild(item5);
 
-    body.appendChild(container);
+    body.appendChild(hoverBarElement);
 
     const dropDownContainer = document.querySelector(".drop-down-container");
-    const hoverBar = document.querySelector(".hover-bar");
 
-    dropDownContainer.addEventListener('mouseover', (e)=>{
-        console.log(e.target.closest(".drop-down-container"));
-        const dropItems = (e.target.closest(".drop-down-container")).querySelectorAll(".drop-down-item");
-        dropItems.forEach((item) =>showElement(item));
+    body.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains("hover-bar")) {
+            const hoverBar = e.target;
+            const dropItems = (hoverBar.closest(".hover-bar")).querySelectorAll(".drop-down-item");
+            dropItems.forEach((item) =>showElement(item));
+        }
     })
 
-    dropDownContainer.addEventListener('mouseout', (e)=>{
-        const dropItems = (e.target.closest(".drop-down-container")).querySelectorAll(".drop-down-item");
-        dropItems.forEach((item) =>hideElement(item));
+
+    const hoverBarGroup = document.querySelectorAll(".hover-bar");
+    hoverBarGroup.forEach((bar) => {
+        bar.addEventListener('mouseleave', (e) => {
+
+            if (e.target.classList.contains("hover-bar")) {
+                const dropItems = (e.target.closest(".hover-bar")).querySelectorAll(".drop-down-item");
+                dropItems.forEach((item) => hideElement(item));
+
+            } 
+        })
     })
+    // hoverBar.addEventListener('mouseout', (e) => {
+    //     if (e.target.classList.contains("hover-bar")) {
+    //         console.log('h')
+    //         const hoverBar = e.target;
+    //         const dropItems = (hoverBar.closest(".hover-bar")).querySelectorAll(".drop-down-item");
+    //         dropItems.forEach((item) => hideElement(item));
+    //     }
+    // })
 
     function hideElement(element) {
         element.classList.add('hidden');
